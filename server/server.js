8c027d87ce6620956362ee7aa1c43a6d639e4ed3
David Lau <avidlaud@gmail.com>
2022-02-19 13:32:24 -0500

    Add cors policy

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const downloadRoute = require('./routes/downloadRoutes');
 
 const app = express();
+app.use(cors());
 
 app.use('/download', downloadRoute);
 
