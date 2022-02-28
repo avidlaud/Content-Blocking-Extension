b4217fe7414ddeabe40a6fd97202a903b4dd7e2e
Milos Seskar <milos.seskar2@gmail.com>
2022-02-27 22:39:03 -0500

    Single image upload, console logs classification or failure

--- predict.js
+++ predict.js
@@ -1,19 +1,44 @@
-//On click of predict button, perform preprocessing of imager and return results
-$("#predict-button").click(async_function () {
-    let image = $("#selected-image").get(0);
-    let tensor = preprocessImage(image);
+let net;
+async function load(){
+    console.log('Loading mobilenet..');
+    // Load the model.
+    net = await mobilenet.load();
+    console.log('Successfully loaded model');
 
-    let predictions = await model.predict(tensor).data();
-    let top5 = Array.from(predictions).map(function (p,i) {
-        return{
-            probability: p,
-            className: IMAGENET_CLASSES[i]
-        };
-    }).sort(function (a,b) {
-        return b.probability - a.probability;
-    }).slice(0,5);
-    $("prediction_list").empty();
-    top5.forEach(function (p) {
-        $("#prediction_list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`)
-    })
-});
+}
+
+
+function uploadImg(input) {
+       if (input.files && input.files[0]) {
+           var reader = new FileReader();
+
+           reader.onload = function (e) {
+               var img = document.getElementById('img');
+               img.src = e.target.result;
+               img.width = 224;
+               img.heigh = 224;
+           };
+
+           reader.readAsDataURL(input.files[0]);
+       }
+
+           app().catch(function(error) {
+               console.log('%cFailed to classify image', 'color:red')
+               console.log(error)
+           });
+
+
+   }
+
+async function app() {
+  // Make a prediction through the model on our image.
+  console.log('Image uploaded successfully')
+  const imgEl = document.getElementById('img'); //grab image from uploaded image selected
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
+
+window.onload = function() {
+    load()
+    // document.getElementById("predict-button").onclick = app;//when the predict button is clicked call function app()
+}
