6c11e59e2f7f299487e237a2add0632dd638627f
David Lau <avidlaud@gmail.com>
2022-03-10 19:43:41 -0500

    Progress

---
+++ extension/src/content_script.js
@@ -0,0 +1,24 @@
+// const port = chrome.runtime.connect({name:"image-channel"});
+
+const findImages = () => {
+    let images = document.getElementsByTagName('img');
+
+    for (const image of images) {
+        // chrome.runtime.sendMessage({ message: 'store_image', img: image.src }, (resp) => {
+        //     console.log(resp);
+        // });
+        chrome.runtime.sendMessage({ message: 'store_image' }, (resp) => {
+            console.log(resp);
+        });
+        // port.postMessage("Found image");
+        image.src = chrome.runtime.getURL('assets/block.png');
+        // Remove the srcset, which is used for responsive images
+        image.srcset = "";
+    }
+}
+
+const observer = new MutationObserver(() => {
+    findImages();
+});
+
+observer.observe(document.body, { subtree: true, childList: true });
