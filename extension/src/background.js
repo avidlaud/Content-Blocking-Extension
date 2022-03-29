01eaf077c683e269383edaf78b1fe31df3bded6e
David Lau <avidlaud@gmail.com>
2022-03-29 00:42:03 -0400

    Classify images
    Image source urls are sent to the background service worker, where they
    are resized and put into the mobilenet model. The response is then sent
    back to the content script and logged.

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,6 +1,5 @@
 import * as tf from '@tensorflow/tfjs'
 import * as mobilenet from '@tensorflow-models/mobilenet'
-import { image } from '@tensorflow/tfjs';
 
 // Implement with async later
 // const handleDownloadRequest = async(data) => {
@@ -8,39 +7,6 @@ import { image } from '@tensorflow/tfjs';
 //     console.log(await (await file).text());
 // }
 
-const handleDownloadRequest = (data) => {
-    const file = fetch(`http://ec2-3-80-69-220.compute-1.amazonaws.com:10000/download/s3?name=${data}`).then((file) => file.text()).then((fileContents) => console.log(fileContents));
-    return "Done!"
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'downloadRequest') {
-        const result = handleDownloadRequest(request.data);
-        sendResponse(result);
-    }
-    return true;
-    // return Promise.resolve("Dummy response");
-});
-
-// Listen for images from the content script
-// const port = chrome.runtime.connect({name:"image-channel"});
-// port.onMessage.addListener((msg) => {
-//     // msg is image
-//     console.log(msg);
-// })
-
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    if (request.message === 'store_image') {
-        const img_response = await fetch(request.img);
-        const img_blob = await img_response.blob();
-        const img_bitmap = await createImageBitmap(img_blob);
-        console.log("Height:" + img_bitmap.height);
-        console.log("Width:" + img_bitmap.width);
-        console.log("Got image");
-    }
-    return true;
-    // return Promise.resolve("Dummy response");
-});
 class ImageClassifier {
     constructor() {
         this.loadModel();
@@ -61,28 +27,45 @@ class ImageClassifier {
             return
         }
         const predictions = await this.model.classify(imageData, 2);
-        console.log(predictions);
+        // console.log(predictions);
+        return predictions;
     }
 }
 
 const imageClassifier = new ImageClassifier();
 
+const handleDownloadRequest = (data) => {
+    const file = fetch(`http://ec2-3-80-69-220.compute-1.amazonaws.com:10000/download/s3?name=${data}`).then((file) => file.text()).then((fileContents) => console.log(fileContents));
+    return "Done!"
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'downloadRequest') {
+        const result = handleDownloadRequest(request.data);
+        sendResponse(result);
+    }
+    return true;
+    // return Promise.resolve("Dummy response");
+});
+
+// Listen for images from the content script
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    if (request.message === 'modelTest') {
-        // Get an image
-        const img = await fetch(chrome.runtime.getURL('dog.png'))
-            .then(data => data.blob())
-            .then(createImageBitmap)
-            .then(imgBitMap => {
-                console.log(imgBitMap);
-                const canvas = new OffscreenCanvas(224, 224);
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(imgBitMap, 0, 0);
-                const arr = Array.from(ctx.getImageData(0, 0, 224, 224).data);
-                const imageData = new ImageData(Uint8ClampedArray.from(arr), 224, 224);
-                return imageData;
-            });
-        const predictions = imageClassifier.analyzeImage(img);
+    if (request.message === 'classify_image') {
+        console.log("Got image");
+        const img_response = await fetch(request.img);
+        const img_blob = await img_response.blob();
+        // Resize image to 224x224 to fit model spec
+        const img_bitmap = await createImageBitmap(img_blob, { resizeHeight: 224, resizeWidth: 224 });
+        const canvas = new OffscreenCanvas(224, 224);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img_bitmap, 0, 0);
+        const arr = Array.from(ctx.getImageData(0, 0, 224, 224).data);
+        const imageData = new ImageData(Uint8ClampedArray.from(arr), 224, 224);
+        const predictions = imageClassifier.analyzeImage(imageData);
+        console.log(await predictions);
+        const classifications = await predictions;
+        const classification = classifications[0];
+        sendResponse({ classification: classification });
     }
     return true;
     // return Promise.resolve("Dummy response");
--- extension/src/content_script.js
+++ extension/src/content_script.js
@@ -1,30 +1,15 @@
-// const port = chrome.runtime.connect({name:"image-channel"});
-
 const findImages = async () => {
-    let images = document.querySelectorAll('img:not(.image-blocked)');
+    let images = document.querySelectorAll('img:not(.image-classified)');
 
     for (const image of images) {
-        // chrome.runtime.sendMessage({ message: 'store_image', img: image.src }, (resp) => {
-        //     console.log(resp);
-        // });
-        // Get the blob
-        // const img_response = await fetch(image.src);
-        // const img_blob = await img_response.blob();
-        // const img_blob_text = await img_blob.text();
-        // console.log(img_blob_text);
-        // const img_bit_map = await createImageBitmap(image);
-        // console.log(img_bit_map);
-        // chrome.runtime.sendMessage({ message: 'store_image', img: img_bit_map.height }, (resp) => {
-        //     console.log(resp);
-        // });
-        chrome.runtime.sendMessage({ message: 'store_image', img: image.src }, (resp) => {
+        chrome.runtime.sendMessage({ message: 'classify_image', img: image.src }, (resp) => {
             console.log(resp);
         });
         // port.postMessage("Found image");
         // image.src = chrome.runtime.getURL('assets/block.png');
         // Remove the srcset, which is used for responsive images
         // image.srcset = "";
-        image.classList.add("image-blocked")
+        image.classList.add("image-classified")
     }
 }
 
