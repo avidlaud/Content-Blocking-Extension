51fc5433a4d70ad3e982b6f97fc25086b00321c2
David Lau <31025257+avidlaud@users.noreply.github.com>
2022-03-30 09:26:59 -0400

    Add linting to extension (#22)
    * Add linting to extension
    
    * Run linter

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,5 @@
-import * as tf from '@tensorflow/tfjs'
-import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 // Implement with async later
 // const handleDownloadRequest = async(data) => {
@@ -11,20 +11,22 @@ class ImageClassifier {
     constructor() {
         this.loadModel();
     }
+
     async loadModel() {
-        this.model = await mobilenet.load({version: 2, alpha: 1.00});
+        this.model = await mobilenet.load({ version: 2, alpha: 1.00 });
         tf.tidy(() => {
             this.model.classify(tf.zeros([1, 224, 224, 3]));
-        })
-        console.log("Model loaded");
+        });
+        console.log('Model loaded');
     }
+
     async analyzeImage(imageData) {
         if (!this.model) {
-            console.log("Waiting for model to load");
+            console.log('Waiting for model to load');
             setTimeout(() => {
                 this.analyzeImage(imageData);
             }, 5000);
-            return
+            return null;
         }
         const predictions = await this.model.classify(imageData, 2);
         // console.log(predictions);
@@ -35,9 +37,9 @@ class ImageClassifier {
 const imageClassifier = new ImageClassifier();
 
 const handleDownloadRequest = (data) => {
-    const file = fetch(`http://ec2-3-80-69-220.compute-1.amazonaws.com:10000/download/s3?name=${data}`).then((file) => file.text()).then((fileContents) => console.log(fileContents));
-    return "Done!"
-}
+    fetch(`http://ec2-3-80-69-220.compute-1.amazonaws.com:10000/download/s3?name=${data}`).then((resp) => resp.text()).then((fileContents) => console.log(fileContents));
+    return 'Done!';
+};
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'downloadRequest') {
@@ -51,21 +53,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 // Listen for images from the content script
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.message === 'classify_image') {
-        console.log("Got image");
-        const img_response = await fetch(request.img);
-        const img_blob = await img_response.blob();
+        console.log('Got image');
+        const imgResponse = await fetch(request.img);
+        const imgBlob = await imgResponse.blob();
         // Resize image to 224x224 to fit model spec
-        const img_bitmap = await createImageBitmap(img_blob, { resizeHeight: 224, resizeWidth: 224 });
+        const imgBitmap = await createImageBitmap(imgBlob, { resizeHeight: 224, resizeWidth: 224 });
         const canvas = new OffscreenCanvas(224, 224);
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(img_bitmap, 0, 0);
+        ctx.drawImage(imgBitmap, 0, 0);
         const arr = Array.from(ctx.getImageData(0, 0, 224, 224).data);
         const imageData = new ImageData(Uint8ClampedArray.from(arr), 224, 224);
         const predictions = imageClassifier.analyzeImage(imageData);
         console.log(await predictions);
         const classifications = await predictions;
         const classification = classifications[0];
-        sendResponse({ classification: classification });
+        sendResponse({ classification });
     }
     return true;
     // return Promise.resolve("Dummy response");
