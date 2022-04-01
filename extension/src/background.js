ce879afad63bc5341cac0216fc637e2d65dd6355
David Lau <avidlaud@gmail.com>
2022-04-01 00:38:56 -0400

    Utilize indexeddb to store models locally
    Currently using MobileNet still but IDB is configured to be able to
    store/pull models.

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,6 @@
 import * as tf from '@tensorflow/tfjs';
-import * as mobilenet from '@tensorflow-models/mobilenet';
+import modelStorage from './modelStorage';
+import IMAGENET_CLASSES from './imagenet_classes';
 
 // Implement with async later
 // const handleDownloadRequest = async(data) => {
@@ -13,10 +14,13 @@ class ImageClassifier {
     }
 
     async loadModel() {
-        this.model = await mobilenet.load({ version: 2, alpha: 1.00 });
-        tf.tidy(() => {
-            this.model.classify(tf.zeros([1, 224, 224, 3]));
-        });
+        const modelPromise = modelStorage.get('mobilenet-model');
+        this.model = await modelPromise;
+        this.model.summary();
+        // Test model
+        const result = tf.tidy(() => this.model.predict(tf.zeros([1, 224, 224, 3])));
+        console.log(await result.data());
+        result.dispose();
         console.log('Model loaded');
     }
 
@@ -28,9 +32,40 @@ class ImageClassifier {
             }, 5000);
             return null;
         }
-        const predictions = await this.model.classify(imageData, 2);
+        // const predictions = await this.model.classify(imageData, 2);
         // console.log(predictions);
-        return predictions;
+        const logits = this.model.predict(this.constructor.normalizeInput(imageData));
+        const softmax = tf.softmax(logits);
+        const values = await softmax.data();
+        // TODO: Below is specific to Mobilenet - change once we switch to a binary classifier
+        const valuesAndIndices = [];
+        for (let i = 0; i < values.length; i += 1) {
+            valuesAndIndices.push({ value: values[i], index: i });
+        }
+        valuesAndIndices.sort((a, b) => b.value - a.value);
+        const topK = 2;
+        const topkValues = new Float32Array(topK);
+        const topkIndices = new Int32Array(topK);
+        for (let i = 0; i < topK; i += 1) {
+            topkValues[i] = valuesAndIndices[i].value;
+            topkIndices[i] = valuesAndIndices[i].index;
+        }
+
+        const topClassesAndProbs = [];
+        for (let i = 0; i < topkIndices.length; i += 1) {
+            topClassesAndProbs.push({
+                className: IMAGENET_CLASSES[topkIndices[i]],
+                probability: topkValues[i],
+            });
+        }
+        console.log(topClassesAndProbs);
+        return topClassesAndProbs;
+    }
+
+    static normalizeInput(imageData) {
+        const img = tf.browser.fromPixels(imageData);
+        const normalized = tf.add(tf.mul(tf.cast(img, 'float32'), (1 / 255.0)), 0);
+        return tf.reshape(normalized, [-1, 224, 224, 3]);
     }
 }
 
