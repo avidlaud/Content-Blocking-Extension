8eaaa2d61b90a66ebd1f1fa5293322168b50a50e
Mark Stanik <mark.stanik@gmail.com>
2022-04-12 20:58:46 -0400

    added cs file, changed some options and added relaxed/strict mode to options

--- extension/src/options.js
+++ extension/src/options.js
@@ -43,3 +43,22 @@ function save_options() {
       list.appendChild(li);
     })*/
   }
+
+const buttonToggleMode = document.getElementById('buttonToggleMode');
+
+chrome.storage.sync.get('strictModeOn', (storage) => {
+    buttonToggleMode.innerText = storage.strictModeOn ? 'Strict Mode' : 'Relaxed Mode';
+});
+
+const toggleMode = () => {
+    chrome.storage.sync.get('strictModeOn', (storage) => {
+        chrome.storage.sync.set({
+            strictModeOn: !storage.strictModeOn,
+        });
+        buttonToggleMode.innerText = !storage.strictModeOn ? 'Strict Mode' : 'Relaxed Mode';
+    });
+};
+
+buttonToggleMode.addEventListener('click', () => {
+    toggleMode();
+});
