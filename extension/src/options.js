daf6ef3c615e1d5ee3f4f094e6b4730beadfdc4f
Mark Stanik <mark.stanik@gmail.com>
2022-04-26 19:54:37 -0400

    options page changes

--- extension/src/options.js
+++ extension/src/options.js
@@ -1,15 +1,19 @@
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
     //creating a list of models
     
     
     chrome.storage.sync.set({
-      favoriteColor: color,
-      likesColor: likesColor
+      
     }, function() {
-        document.body.style.background = color;
       // Update status to let user know options were saved.
+      const revealImage = document.querySelector('#revealImage');
+      if(revealImage.checked){
+
+      }
+      else{
+
+      }
+
       var status = document.getElementById('status');
       status.textContent = 'Options saved.';
       setTimeout(function() {
@@ -23,27 +27,17 @@ function save_options() {
   function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
-      favoriteColor: 'red',
-      likesColor: true
+      //favoriteColor: 'red',
+      //likesColor: true
     }, function(items) {
-      document.getElementById('color').value = items.favoriteColor;
-      document.getElementById('like').checked = items.likesColor;
+      //document.getElementById('color').value = items.favoriteColor;
+      //document.getElementById('like').checked = items.likesColor;
     });
   }
   document.addEventListener('DOMContentLoaded', restore_options);
   document.getElementById('save').addEventListener('click',
       save_options);
   
-  function get_models() {
-    //var models = downloadController.listModels;
-    /*let list = document.getElementById("listModels");
-    models.forEach((item)=>{
-      let li = document.createElement("li");
-      li.innerText = item;
-      list.appendChild(li);
-    })*/
-  }
-
 const buttonToggleMode = document.getElementById('buttonToggleMode');
 
 chrome.storage.sync.get('strictModeOn', (storage) => {
@@ -61,4 +55,52 @@ const toggleMode = () => {
 
 buttonToggleMode.addEventListener('click', () => {
     toggleMode();
+});
+
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+inpFile.addEventListener("change", function(){
+  const file = this.files[0];
+  //console.log(file);
+  if(file){
+    const reader = new FileReader();
+    console.log("aaaa")
+    previewDefaultText.style.display = "none";
+    console.log("bbbb")
+    previewImage.style.display = "block";
+    console.log("GONNA ERROR")
+    reader.addEventListener("load", function(){
+      console.log("HELLLLLLLO")
+      previewImage.setAttribute("src", this.result);
+    });
+  
+    reader.readAsDataURL(file);
+  } else{
+    console.log(file)
+    previewDefaultText.style.display = "null";
+    previewImage.style.display = "null";
+    previewImage.setAttribute("src", "")
+
+
+  }
+});
+let models = [
+  'Blue',
+  'Red',
+  'White',
+  'Green',
+  'Black',
+  'Orange'
+],
+select = document.createElement('select');
+document.getElementById('modelList').appendChild(select);
+
+models.forEach(function (models) {
+    let option = document.createElement('option');
+    select.appendChild(option);
+
+    option.innerHTML += models;
 });
